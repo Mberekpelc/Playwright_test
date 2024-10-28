16f3117eaa5858d0034483834b23157ecd849eb7
Lawrence <mblaw73@gmail.com>
2024-10-28 11:51:56 +0000

    added more aserssion

--- tests/accessibility_test.spec.ts
+++ tests/accessibility_test.spec.ts
@@ -5,16 +5,19 @@ import AxeBuilder from '@axe-core/playwright';
 test("basics accessibility", async ({page}, testInfo)=>{
    await page.goto("https://commitquality.com/practice-api");
    await page.locator('.back-link').waitFor();
-   const axeBuilder = await new AxeBuilder ({page})
-   //.include('.back-link') to test a specific element or part of the page
-   .withTags(["wcag2a","wcag2aa","wcag21a","wcag21aa"])
-   .disableRules(["region"])
-   .exclude('#element-with-known-issue')
-   .analyze();
-   //to add result to json
-   await testInfo.attach('accessibility-scan-results', {
-      body: JSON.stringify(axeBuilder, null, 2),
-      contentType: 'application/json'
-    });
-   expect(axeBuilder.violations).toEqual([]);
+   await test.step("check a11y", async ()=>{
+      const axeBuilder = await new AxeBuilder ({page})
+      //.include('.back-link') to test a specific element or part of the page
+      .withTags(["wcag2a","wcag2aa","wcag21a","wcag21aa"])
+      .disableRules(["region"])
+      .exclude('#element-with-known-issue')
+      .analyze()
+      //to add result to json
+      await testInfo.attach('accessibility-scan-results', {
+         body: JSON.stringify(axeBuilder, null, 2),
+         contentType: 'application/json'
+       });
+      expect(axeBuilder.violations.length).toEqual(0);
+      expect(axeBuilder.violations).toEqual([]);
+   })
 });
